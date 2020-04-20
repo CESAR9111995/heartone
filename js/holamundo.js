3cc2e8d0c3d18b08c78189bd33071f68a5bf80b7
CESAR9111995 <cesar1995.ch@gmail.com>
2020-04-19 20:22:53 -0500

    mi repositorio

---
+++ js/holamundo.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var numero_entero= 17;
+
+switch(numero_entero){
+
+	case (numero_entero <= 18):
+             document.write("Eres mayor de edad");
+             break;
+
+
+	case 35:
+             document.write("Eres adulto");
+             break;
+
+
+	case 80:
+             document.write("Eres de la tercera edad");
+             break;
+
+             default:
+              document.write("No coincide con ninguna edad");
+
+}
